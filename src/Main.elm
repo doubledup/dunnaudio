1040a83a6d3ae4a833e4dfa5c8fe587a016c0403
David Dunn <26876072+doubledup@users.noreply.github.com>
2023-10-28 12:03:25 +0200

    Set up initial page

---
+++ src/Main.elm
@@ -0,0 +1,61 @@
+module Main exposing (main)
+
+import Browser
+import Browser.Navigation exposing (Key)
+import Html exposing (..)
+import Url
+
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = onUrlChange
+        , onUrlRequest = onUrlRequest
+        }
+
+
+type alias Model =
+    {}
+
+
+init : flags -> Url.Url -> Key -> ( Model, Cmd Msg )
+init _ _ _ =
+    ( {}, Cmd.none )
+
+
+type Msg
+    = Noop
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Noop ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+onUrlRequest : Browser.UrlRequest -> Msg
+onUrlRequest _ =
+    Noop
+
+
+onUrlChange : Url.Url -> Msg
+onUrlChange _ =
+    Noop
+
+
+view : Model -> Browser.Document Msg
+view _ =
+    { title = "Dunnaudio"
+    , body =
+        [ Html.h1 [] [ Html.text "Dunnaudio" ] ]
+    }
